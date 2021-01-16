457595dece9830bd47221523297cd856572b816e
Nunes <GilsonNunes_dev@outlook.com>
2021-01-16 20:47:40 -0300

    added RegistrationMail.js and event configuration

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,4 @@
 import passwordGenerator from "password-generator";
-import Mail from "../lib/Mail";
 
 export default {
   async store(req, res) {
@@ -10,13 +9,6 @@ export default {
       email,
       password: passwordGenerator(15, false),
     };
-
-    await Mail.sendMail({
-      from: "DIO <contato@dio.com>",
-      to: `${name} <${email}>`,
-      subject: "Cadastro de usuário",
-      html: `Olá, ${name}, bem-vindo a DIO.`,
-    });
 
     return res.json(user);
   },
